ded77013af67759bb8f71248717cc96ccd166af2
dosimont <damien.dosimont@imag.fr>
2013-03-21 12:48:00 +0100

    minor modif

--- Release/src/subdir.mk
+++ Release/src/subdir.mk
@@ -9,15 +9,11 @@ CPP_SRCS += \
 ../src/ScalarLPAggreg.cpp \
 ../src/VectorLPAggreg.cpp 
 
-CXX_SRCS += \
-../src/lpaggreg_wrap.cxx 
-
 OBJS += \
 ./src/LPAggreg.o \
 ./src/LPAggregWrapper.o \
 ./src/ScalarLPAggreg.o \
-./src/VectorLPAggreg.o \
-./src/lpaggreg_wrap.o 
+./src/VectorLPAggreg.o 
 
 CPP_DEPS += \
 ./src/LPAggreg.d \
@@ -25,22 +21,12 @@ CPP_DEPS += \
 ./src/ScalarLPAggreg.d \
 ./src/VectorLPAggreg.d 
 
-CXX_DEPS += \
-./src/lpaggreg_wrap.d 
-
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/java/latest/include -I/usr/java/latest/include/linux -O1 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
-src/%.o: ../src/%.cxx
-	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/java/latest/include -I/usr/java/latest/include/linux -O1 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O1 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
