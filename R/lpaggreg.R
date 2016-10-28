50cb1f3d5c418894c59d63d1d34a25a970f6c141
Damien Dosimont <damien.dosimont@bsc.es>
2016-10-28 11:08:30 +0200

    improving script

--- R/lpaggreg.R
+++ R/lpaggreg.R
@@ -140,20 +140,32 @@ testArray[1,2,] = c(0.5, 0.8, 1.0, 0.9, 0.6)
 testArray[2,1,] = c(0.4, 0.3, 0.1, 0.2, 0.3)
 testArray[2,2,] = c(0.6, 0.7, 0.9, 0.8, 0.7)
 print(testArray)
+
 #Threshold: 0<th<1, lower value means more accuracy for retrieving the list of partitions but longer computation time
 th=0.001
-#Output: a 2D matrix (n,3) with the list of parts for each parameter
+
+#Temporal aggregation
+#Output: a 2D matrix (n,3) with the list of time parts for each parameter
 #Columns: parameter p, start timeslice, end timeslice
 oaggregate(testArray, th)
+
+#Spatial aggregation
 #define hierarchy: vector[index]= index's father index; vector[index]0 means index=root
 h=c(3,3,0)
+#Output: a 2D matrix (n,3) with the list of aggregate nodes
 #Columns: parameter p, node, size
 haggregate(testArray, h, th)
+
+#Spatiotemporal aggregation
+#Output: a 2D matrix (n,5) with the list of aggregate nodes/time parts
 #Columns: parameter p, node, size, start, end
 daggregate(testArray, h, th)
 
+#Real trace example
 dataCube=parsePJDump("cholesky_11520_960_starpu_25_3_dmda_1_idcin-2.grenoble.grid5000.fr_2016-08-21_20-49-12_pjdump.csv", 20)
 
+#Show the micromodel
 dataCube
 
+#Temporal aggregation
 oaggregate(dataCube, th)
