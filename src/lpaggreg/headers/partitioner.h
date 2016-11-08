a4bf8624d8ff459849ad18313b776ebf911063b1
Damien Dosimont <damien.dosimont@bsc.es>
2016-11-08 20:02:21 +0100

    correcting small bugs + curve generation in R script

--- src/lpaggreg/headers/partitioner.h
+++ src/lpaggreg/headers/partitioner.h
@@ -16,10 +16,11 @@ namespace lpaggreg{
     public:
         Partitioner();
         virtual void computeBestPartitions(float threshold, float min=0.0, float max=1.0)=0;
-        map<float, shared_ptr<Quality> > getQualityList();
         vector<float> getPList() const;
         float getP(int strategy);
         float computeAUC();
+
+        map<float, shared_ptr<Quality> > getQualityList() const;
 
     protected:
         vector<float> pList;
